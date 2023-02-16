9ca7b316bffe4d68ef4e80b7a514c9c70241b7d2
Jhamie-Digital <jamessundayo30@gmail.com>
2023-02-16 14:58:37 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - writing a code that is betting compliance
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
